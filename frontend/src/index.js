55279d2c51e2f13fbfa11f53db11cdda59c609c9
mpalmer79 <mpalm2025@gmail.com>
2025-12-02 15:10:58 -0500

    Simplify render by removing unnecessary wrappers
    Removed BrowserRouter and KioskProvider wrappers from the render method.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { KioskProvider } from './context/KioskContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <KioskProvider>
-        <App />
-      </KioskProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
