293b3139ed3834168e138ffc63518f6a5ce0c48f
mpalmer79 <mpalm2025@gmail.com>
2025-12-02 15:10:16 -0500

    Refactor App component to use KioskApp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useKiosk } from './context/KioskContext';
-import Header from './components/Header';
-import Screensaver from './components/Screensaver';
-import HomePage from './pages/HomePage';
-import VehicleDetailPage from './pages/VehicleDetailPage';
-import SearchPage from './pages/SearchPage';
+import KioskApp from './components/Kioskapp';
 
 function App() {
-  const { isIdle, wakeUp } = useKiosk();
-
-  return (
-    <div className="app-container" onClick={wakeUp}>
-      {isIdle && <Screensaver onTouch={wakeUp} />}
-      
-      <Header />
-      
-      <main className="page-container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/vehicle/:id" element={<VehicleDetailPage />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Routes>
-      </main>
-    </div>
-  );
+  return <KioskApp />;
 }
 
 export default App;
