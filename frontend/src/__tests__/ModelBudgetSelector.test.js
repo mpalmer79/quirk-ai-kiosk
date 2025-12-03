fe485f734c2cc015ef99d230f969921cc6aabebb
mpalmer79 <mpalm2025@gmail.com>
2025-12-03 17:24:42 -0500

    Fix import path for ModelBudgetSelector component

--- frontend/src/__tests__/ModelBudgetSelector.test.js
+++ frontend/src/__tests__/ModelBudgetSelector.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import ModelBudgetSelector from '../components/Modelbudgetselector';
+import ModelBudgetSelector from '../components/ModelBudgetSelector';
 
 // Mock the api module
 jest.mock('../components/api', () => ({
