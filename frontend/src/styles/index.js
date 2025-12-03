d31ee12da4784fde881d11e023ad41255d7dfb17
mpalmer79 <mpalm2025@gmail.com>
2025-12-03 11:53:02 -0500

    Create styles barrel export in index.js

---
+++ frontend/src/styles/index.js
@@ -0,0 +1,10 @@
+/**
+ * Styles barrel export
+ * Import all styles from this single file
+ */
+
+export { default as theme } from './theme';
+export * from './theme';
+
+export { default as commonStyles } from './commonStyles';
+export * from './commonStyles';
