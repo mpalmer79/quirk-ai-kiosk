51de9f390aa7c04874e2e9df118be0c191f2f5ab
mpalmer79 <mpalm2025@gmail.com>
2025-12-09 16:51:14 -0500

    Update playwright.sonfig.ts

--- frontend/playwright.sonfig.ts
+++ frontend/playwright.sonfig.ts
@@ -2,6 +2,8 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
+  testMatch: '**/*.spec.ts',
+  testIgnore: ['**/src/**', '**/node_modules/**'],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
