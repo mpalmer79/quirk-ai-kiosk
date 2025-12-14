11672dca71d87c86024fc66ff187111e07db491e
mpalmer79 <mpalm2025@gmail.com>
2025-12-14 15:22:25 -0500

    Update SuggestionChips.tsx

--- frontend/src/components/AIAssistant/components/SuggestionChips.tsx
+++ frontend/src/components/AIAssistant/components/SuggestionChips.tsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import { styles } from '../styles';
+import { SUGGESTED_PROMPTS } from '../constants';
 
+interface SuggestionChipsProps {
+  onSelect: (prompt: string) => void;
+}
+
+export const SuggestionChips: React.FC<SuggestionChipsProps> = ({ onSelect }) => {
+  return (
+    <div style={styles.suggestionsContainer}>
+      <p style={styles.suggestionsLabel}>Try asking me:</p>
+      <div style={styles.suggestionsGrid}>
+        {SUGGESTED_PROMPTS.map((prompt, idx) => (
+          <button
+            key={idx}
+            style={styles.suggestionButton}
+            onClick={() => onSelect(prompt)}
+          >
+            {prompt}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SuggestionChips;
