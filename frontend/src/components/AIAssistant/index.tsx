b9306671749c71afdbd0032472e172b7f54baafd
mpalmer79 <mpalm2025@gmail.com>
2025-12-14 15:17:29 -0500

    Update index.tsx

--- frontend/src/components/AIAssistant/index.tsx
+++ frontend/src/components/AIAssistant/index.tsx
@@ -1,1 +1,5 @@
-
+export { default } from './AIAssistant';
+export { default as AIAssistant } from './AIAssistant';
+export * from './types';
+export * from './constants';
+export { styles } from './styles';
