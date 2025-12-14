7be218412ab13efb888c9c35e9ab5fc95d5992bb
mpalmer79 <mpalm2025@gmail.com>
2025-12-14 15:21:27 -0500

    Update Header.tsx

--- frontend/src/components/AIAssistant/components/Header.tsx
+++ frontend/src/components/AIAssistant/components/Header.tsx
@@ -1,1 +1,7 @@
-
+export { Header } from './Header';
+export { MessageBubble } from './MessageBubble';
+export { VehicleCard } from './VehicleCard';
+export { SuggestionChips } from './SuggestionChips';
+export { ObjectionPanel } from './ObjectionPanel';
+export { ChatInput } from './ChatInput';
+export { LoadingIndicator } from './LoadingIndicator';
