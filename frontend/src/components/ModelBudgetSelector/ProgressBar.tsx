5b9874fd396d119adb3d83126f8aaf7d6198876c
mpalmer79 <mpalm2025@gmail.com>
2025-12-30 16:06:16 -0500

    Update ProgressBar.tsx

--- frontend/src/components/ModelBudgetSelector/ProgressBar.tsx
+++ frontend/src/components/ModelBudgetSelector/ProgressBar.tsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import styles from '../modelBudgetSelectorStyles';
 
+interface ProgressBarProps {
+  currentStep: number;
+  totalSteps: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ currentStep, totalSteps }) => {
+  return (
+    <div style={styles.progressBar}>
+      {Array.from({ length: totalSteps }, (_, i) => (
+        <div 
+          key={i} 
+          style={{
+            ...styles.progressDot,
+            ...(i + 1 <= currentStep ? styles.progressDotActive : {}),
+            ...(i + 1 < currentStep ? styles.progressDotComplete : {}),
+          }} 
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ProgressBar;
