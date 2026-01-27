bef08fa76541273ec57961e21dccdd239f6b03d6
mpalmer79 <mpalm2025@gmail.com>
2026-01-27 14:41:46 -0500

    Update index.ts

--- frontend/src/components/SalesManagerDashboard/components/index.ts
+++ frontend/src/components/SalesManagerDashboard/components/index.ts
@@ -1,2 +1,7 @@
-export { default } from './SalesManagerDashboard';
-export * from './types';
+export { SessionList } from './SessionList';
+export { SessionCard } from './SessionCard';
+export { WorksheetList } from './WorksheetList';
+export { WorksheetCard } from './WorksheetCard';
+export { WorksheetDetail } from './WorksheetDetail';
+export { ChatTranscript } from './ChatTranscript';
+export { Placeholder } from './Placeholder';
